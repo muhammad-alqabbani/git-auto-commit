dcd5f2bd59d5357edbe4a7e25912775e2f5aaef8
muhammad-alqabbani <62672543+muhammad-alqabbani@users.noreply.github.com>
2024-04-03 19:41:48 +0000

    Add support for Multi-root Workspaces in Visual Studio Code

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 // this method is called when your extension is activated
-import Scheduler from './Scheduler';
-import { checkPathSafe, getConfig } from './utils';
-let instance:Scheduler;
+import Scheduler from './Scheduler'
+import { checkPathSafe, getConfig } from './utils'
+
+let instances: { [path: string]: Scheduler } = {}
 
 export async function activate(context: vscode.ExtensionContext) {
-	let config:Option = {
-		commitTimeInterval:getConfig<number>('commitTimeInterval')|| 0,
-		autoPush:getConfig<boolean>('autoPush') || false,
-		context,
-		path: ''
-	};
-	let checkPathRes = await checkPathSafe(vscode.workspace.workspaceFolders);
-	if(checkPathRes){
-		vscode.window.showErrorMessage(checkPathRes);
-		return;
-	}else{
-		config.path = vscode.workspace.workspaceFolders?.[0].uri.fsPath as string;
-		instance = new Scheduler({...config,context});
-		vscode.window.showInformationMessage('git-auto-commit成功启用');
-		vscode.workspace.onDidSaveTextDocument((e)=> instance.changeListener(e));
-	}
-	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => {
-		config.commitTimeInterval = getConfig<number>('commitTimeInterval') ||0;
-		config.autoPush = getConfig<boolean>('autoPush') || false;
-		instance.changeOptions(config);
-	}));
+    if (vscode.workspace.workspaceFolders) {
+        for (const folder of vscode.workspace.workspaceFolders) {
+            let config: Option = {
+                commitTimeInterval: getConfig<number>('commitTimeInterval') || 0,
+                autoPush: getConfig<boolean>('autoPush') || false,
+                context,
+                path: folder.uri.fsPath,
+            }
+            let checkPathRes = await checkPathSafe([folder])
+            if (checkPathRes) {
+                vscode.window.showErrorMessage(checkPathRes)
+                return
+            } else {
+                instances[folder.uri.fsPath] = new Scheduler(config)
+            }
+        }
+    }
+
+    vscode.window.showInformationMessage('git-auto-commit successfully enabled')
+
+    vscode.workspace.onDidSaveTextDocument((e) => {
+        const folder = vscode.workspace.getWorkspaceFolder(e.uri)
+        if (folder) {
+            instances[folder.uri.fsPath].changeListener(e)
+        }
+    })
+
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeConfiguration(() => {
+            for (const path in instances) {
+                let config: Option = {
+                    commitTimeInterval: getConfig<number>('commitTimeInterval') || 0,
+                    autoPush: getConfig<boolean>('autoPush') || false,
+                    context,
+                    path,
+                }
+                instances[path].changeOptions(config)
+            }
+        })
+    )
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-	instance.destroy();
+    for (const path in instances) {
+        instances[path].destroy()
+    }
 }
